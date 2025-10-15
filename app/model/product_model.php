717df6c54f3cfe4dcf604618ac0552d8a3b60d7f
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-10-15 16:36:45 -0300

    Se suma phtml & component + Bootstrap
    phtml+Bootstrap

--- app/model/product_model.php
+++ app/model/product_model.php
@@ -6,13 +6,29 @@ class ProductModel {
 
     $db = new PDO('mysql:host=localhost;dbname=db_perfumeria;charset=utf8', 'root', '');
 
-    $query = $db->prepare( "select * from producto"); //Metodo para traer productos
+    $query = $db->prepare( "select * from producto"); //Metodo para traer productos (Listado de ítems: Se debe poder visualizar todos los ítems cargados)
     $query->execute();
 
     $arrayProduct = $query->fetchAll(PDO::FETCH_OBJ);
 
         return $arrayProduct;
 } 
+
+    function getProductId($id) //Metodo para traer detalle de ítem: Se debe poder navegar y visualizar cada ítem particularmente 
+
+    { 
+          $db = new PDO('mysql:host=localhost;dbname=db_perfumeria;charset=utf8', 'root', '');
+
+        $query = $db->prepare( "select * from producto WHERE id = ?"); //Metodo para traer productos (Listado de ítems: Se debe poder visualizar todos los ítems cargados)
+        $query->execute($id);
+
+        $arrayProduct = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $arrayProduct;
+
+     }
+
+
 
 
 }

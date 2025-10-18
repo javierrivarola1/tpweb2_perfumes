42e8ee32805fb29a786dea0fdbc527a1a58d658a
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-10-18 09:33:23 -0300

    Nueva base de datos
    Nueva base de datos

--- db_perfumeria.sql
+++ db_perfumeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2025 a las 00:39:21
+-- Tiempo de generación: 18-10-2025 a las 14:30:40
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `marca` (
   `id` int(11) NOT NULL,
-  `nombre` text NOT NULL,
+  `nombre` varchar(50) NOT NULL,
   `origen` varchar(25) DEFAULT NULL,
   `creador` varchar(50) DEFAULT NULL,
   `año` int(4) DEFAULT NULL
@@ -52,8 +52,9 @@ INSERT INTO `marca` (`id`, `nombre`, `origen`, `creador`, `año`) VALUES
 
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
-  `nombre` text NOT NULL,
-  `descripcion` text NOT NULL,
+  `url` varchar(1000) NOT NULL,
+  `nombre` varchar(500) NOT NULL,
+  `descripcion` varchar(700) NOT NULL,
   `marca` int(100) NOT NULL,
   `sexo` char(3) NOT NULL,
   `stock` tinyint(1) NOT NULL,
@@ -65,16 +66,32 @@ CREATE TABLE `producto` (
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `marca`, `sexo`, `stock`, `precio`, `presentacion`) VALUES
-(1, 'Blue Seduction Antonio', 'Blue Seduction de Antonio Banderas es una fragancia de la familia olfativa Oriental Fougère para Hombres. Las Notas de Salida son melón, bergamota, menta y grosellas negras; las Notas de Corazón son agua de mar, manzana verde, capuchino, cardamomo y nuez moscada; las Notas de Fondo son notas amaderadas y ámbar.', 1, 'M', 1, 15000, 120),
-(2, 'King of Seduction Absolute', 'King of Seduction Absolute de Antonio Banderas es una fragancia de la familia olfativa Amaderada Aromática para Hombres.\r\n Las Notas de Salida son toronja (pomelo), toronja (pomelo), ajenjo (absenta) y almendras verdes; las Notas de Corazón son notas marinas, lavanda y cardamomo; las Notas de Fondo son cuero, vetiver y musgo de roble.', 1, 'M', 1, 16000, 60),
-(3, 'King of Seduction Antonio Banderas', 'King of Seduction de Antonio Banderas es una fragancia de la familia olfativa Amaderada Aromática para Hombres. Las Notas de Salida son piña, bergamota, melón, manzana verde y toronja (pomelo); las Notas de Corazón son notas marinas, jazmín, cardamomo y neroli; las Notas de Fondo son vetiver, almizcle blanco, cedro, gamuza y ámbar.', 1, 'M', 1, 16000, 60),
-(4, 'Good Girl', 'Good Girl de Carolina Herrera es una fragancia de la familia olfativa Oriental Floral para Mujeres. Las Notas de Salida son almendra, café, bergamota y limón (lima ácida); las Notas de Corazón son nardos, jazmín sambac (sampaguita), flor de azahar del naranjo, rosa de Bulgaria (rosa Damascena de Bulgaria) y raíz de lirio; las Notas de Fondo son haba tonka, cacao, vainilla, praliné, sándalo, almizcle, ámbar, madera de cachemira, pachulí, canela y cedro.', 3, 'F', 1, 28000, 100),
-(5, '212 VIP Rosé', '212 VIP Rosé de Carolina Herrera es una fragancia de la familia olfativa Floral Frutal para Mujeres.  Las Notas de Salida son Champagne Rosé y pimienta rosa; las Notas de Corazón son flor del duraznero (flor del melocotonero) y rosa; las Notas de Fondo son almizcle blanco y notas amaderadas.', 3, 'F', 1, 20000, 50),
-(6, 'Carolina', 'Carolina de Carolina Herrera es una fragancia de la familia olfativa Almizcle Floral Amaderado para Mujeres. Las Notas de Salida son hojas del fresal salvaje, naranja amarga y cardamomo; las Notas de Corazón son frutas del bosque, rosa y pimienta; las Notas de Fondo son madera de cachemira, almizcle, ámbar y vainilla.', 3, 'F', 1, 25000, 100),
-(7, 'Invictus Victory Elixir Rabanne', 'Invictus Victory Elixir de Rabanne es una fragancia de la familia olfativa Oriental Amaderada para Hombres. Esta fragrancia es nueva. Invictus Victory Elixir se lanzó en 2023. Las Notas de Salida son lavanda, cardamomo y pimienta negra; las Notas de Corazón son incienso y pachulí; las Notas de Fondo son vaina de vainilla y haba tonka.', 2, 'M', 1, 55000, 50),
-(8, 'Olympéa Rabanne', 'Olympéa de Rabanne es una fragancia de la familia olfativa Oriental Floral para Mujeres. Las Notas de Salida son jazmín de agua, mandarina verde y flor de jengibre; las Notas de Corazón son vainilla y sal; las Notas de Fondo son madera de cachemira, ámbar gris y sándalo.', 2, 'F', 1, 21000, 50),
-(9, '1 Million Royal Rabanne', '1 Million Royal de Rabanne es una fragancia de la familia olfativa Oriental Amaderada para Hombres.  Las Notas de Salida son cardamomo, naranja tangerina y bergamota; las Notas de Corazón son lavanda, salvia y hojas de violeta; las Notas de Fondo son benjuí, cedro y pachulí.', 2, 'U', 1, 23000, 100);
+INSERT INTO `producto` (`id`, `url`, `nombre`, `descripcion`, `marca`, `sexo`, `stock`, `precio`, `presentacion`) VALUES
+(4, 'https://juleriaque.vtexassets.com/arquivos/ids/201518/good-girl-edp-8FB3ED41E09E468530B3404297CDA472.jpg?v=638085739653070000', 'Good Girl', 'Good Girl de Carolina Herrera es una fragancia de la familia olfativa Oriental Floral para Mujeres. Las Notas de Salida son almendra, café, bergamota y limón (lima ácida); las Notas de Corazón son nardos, jazmín sambac (sampaguita), flor de azahar del naranjo, rosa de Bulgaria (rosa Damascena de Bulgaria) y raíz de lirio; las Notas de Fondo son haba tonka, cacao, vainilla, praliné, sándalo, almizcle, ámbar, madera de cachemira, pachulí, canela y cedro.', 3, 'F', 1, 28000, 100),
+(6, 'https://m.media-amazon.com/images/I/61q5qvBaZ9L.jpg', 'Carolina', 'Carolina de Carolina Herrera es una fragancia de la familia olfativa Almizcle Floral Amaderado para Mujeres. Las Notas de Salida son hojas del fresal salvaje, naranja amarga y cardamomo; las Notas de Corazón son frutas del bosque, rosa y pimienta; las Notas de Fondo son madera de cachemira, almizcle, ámbar y vainilla.', 3, 'F', 1, 25000, 100),
+(7, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuRWLKyEI1TerN0OBO7zVn4vRZ7wCWJC1JxQ&s', 'Invictus Victory Elixir Rabanne', 'Invictus Victory Elixir de Rabanne es una fragancia de la familia olfativa Oriental Amaderada para Hombres. Esta fragrancia es nueva. Invictus Victory Elixir se lanzó en 2023. Las Notas de Salida son lavanda, cardamomo y pimienta negra; las Notas de Corazón son incienso y pachulí; las Notas de Fondo son vaina de vainilla y haba tonka.', 2, 'M', 1, 55000, 50),
+(8, 'https://www.siemprefarmacias.com.ar/contenido/productos/original/1631298985-7750.jpeg', 'Olympéa Rabanne', 'Olympéa de Rabanne es una fragancia de la familia olfativa Oriental Floral para Mujeres. Las Notas de Salida son jazmín de agua, mandarina verde y flor de jengibre; las Notas de Corazón son vainilla y sal; las Notas de Fondo son madera de cachemira, ámbar gris y sándalo.', 2, 'F', 1, 21000, 50),
+(9, 'https://farmaciasdelpueblo.vtexassets.com/arquivos/ids/209568/3349668617050-2.png?v=638848256100330000', '1 Million Royal Rabanne', '1 Million Royal de Rabanne es una fragancia de la familia olfativa Oriental Amaderada para Hombres.  Las Notas de Salida son cardamomo, naranja tangerina y bergamota; las Notas de Corazón son lavanda, salvia y hojas de violeta; las Notas de Fondo son benjuí, cedro y pachulí.', 2, 'U', 1, 23000, 100),
+(13, 'https://www.siemprefarmacias.com.ar/contenido/productos/original/1659099276-320.jpeg', 'King of Seduction Absolute', 'Fragancia de la familia olfativa Amaderada Aromática para Hombres. Las Notas de Salida son piña, melón, bergamota, manzana verde y toronja ', 1, 'M', 1, 13000, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario` varchar(80) NOT NULL,
+  `contrasenia` varchar(80) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario`, `contrasenia`, `id`) VALUES
+('webadmin', 'admin', 1);
 
 --
 -- Índices para tablas volcadas
@@ -85,15 +102,21 @@ INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `marca`, `sexo`, `stock`,
 --
 ALTER TABLE `marca`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`) USING HASH;
+  ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`) USING HASH,
+  ADD UNIQUE KEY `nombre` (`nombre`),
   ADD KEY `marca` (`marca`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -109,7 +132,13 @@ ALTER TABLE `marca`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
